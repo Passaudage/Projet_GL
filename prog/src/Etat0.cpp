4de2f30539c1c11de0d6a7db92eaaea96bc45f1e
Quentin <quentin.labernia@insa-lyon.fr>
2016-03-11 11:40:40 +0100

    make

--- prog/src/Etat0.cpp
+++ prog/src/Etat0.cpp
@@ -15,7 +15,7 @@ Etat0::~Etat0()
 void Etat0::transition(Automate* a, Symbole* s)
 {
 	switch(*s){
-		case SYMBOLE_D:
+		case DECLARATIONS:
 			a->pushSymbole(s);
 			a->pushEtat(new Etat0());
 			break;
