61f26ef7656070e71c8a14e051a36bfab4387422
RastaMarcusLeRouge <javin.marc@gmail.com>
2016-03-11 11:20:01 +0100

    fichiers symboles

---
+++ prog/src/script.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+touch Symbole.cpp Programme.cpp Declarations.cpp Expression.cpp ExpressionBinaire.cpp Identifiant.cpp Valeur.cpp 
+touch ExpressionParenthesee.cpp ExpressionSoustraction.cpp ExpressionAddition.cpp ExpressionDivision.cpp 
+touch ExpressionMultiplication.cpp DeclarationVariable.cpp Declaration.cpp DeclarationVariable.cpp DevlarationConstante.cpp Instructions.cpp Instruction.cpp Lecture.cpp InstructionExpression.cpp Affichage.cpp Affectation.cpp ParentheseOuvrante.cpp ParentheseFermante.cpp OperateurMult.cpp OperateurAdd.cpp PointVirgule.cpp Virgule.cpp DeuxPointsEgal.cpp Egal.cpp
+
