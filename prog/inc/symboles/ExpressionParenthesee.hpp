edb1513aedbe2d57f39b5fd534fdf2f5109db493
Luc Forget <luc.forget@insa-lyon.fr>
2016-03-13 17:47:24 +0100

    Modifications

---
+++ prog/inc/symboles/ExpressionParenthesee.hpp
@@ -0,0 +1,70 @@
+/*************************************************************************
+                           ExpressionParenthesee  -  description
+                             -------------------
+    début                : dim. 13 mars 2016
+*************************************************************************/
+
+// Interface de la classe <ExpressionParenthesee> 
+// (fichier ExpressionParenthesee.hpp) ------
+#ifndef EXPRESSIONPARENTHESEE_H
+#define EXPRESSIONPARENTHESEE_H
+
+//--------------------------------------------------- Interfaces utilisées
+#include "symboles/Expression.hpp"
+//------------------------------------------------------------- Constantes 
+
+//------------------------------------------------------------------ Types 
+
+//------------------------------------------------------------------------ 
+// Rôle de la classe <ExpressionParenthesee>
+//
+//
+//------------------------------------------------------------------------ 
+
+class ExpressionParenthesee : public Expression
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- Méthodes publiques
+	int calculer(Programme & programme);
+
+//------------------------------------------------- Surcharge d'opérateurs
+    ExpressionParenthesee& operator=(
+			const ExpressionParenthesee & unExpressionParenthesee) = delete;
+
+//-------------------------------------------- Constructeurs - destructeur
+    ExpressionParenthesee(
+			const ExpressionParenthesee & unExpressionParenthesee) = delete;
+
+    ExpressionParenthesee(Expression& exprInter);
+
+    ~ExpressionParenthesee();
+
+//------------------------------------------------------------------ PRIVE 
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+private:
+//------------------------------------------------------- Méthodes privées
+
+protected:
+//----------------------------------------------------- Attributs protégés
+	Expression& _exprInter;
+
+private:
+//------------------------------------------------------- Attributs privés
+
+//---------------------------------------------------------- Classes amies
+
+//-------------------------------------------------------- Classes privées
+
+//----------------------------------------------------------- Types privés
+
+};
+
+//----------------------------------------- Types dépendants de <ExpressionParenthesee>
+
+#endif // EXPRESSIONPARENTHESEE_H
+
