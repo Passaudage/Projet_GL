982794b8867e66344d1b772d5ef34efe64028853
Luc Forget <luc.forget@insa-lyon.fr>
2016-03-15 15:32:37 +0100

    Instruction

---
+++ prog/inc/symboles/Instruction.hpp
@@ -0,0 +1,67 @@
+/*************************************************************************
+                           Instruction  -  description
+                             -------------------
+    début                : mar. 15 mars 2016
+*************************************************************************/
+
+//--------- Interface de la classe <Instruction> (fichier Instruction.hpp) ------
+#ifndef INSTRUCTION_H
+#define INSTRUCTION_H
+
+//--------------------------------------------------- Interfaces utilisées
+#include "Symbole.hpp"
+
+//------------------------------------------------------------- Constantes 
+
+//------------------------------------------------------------------ Types 
+
+//------------------------------------------------------------------------ 
+// Rôle de la classe <Instruction>
+//
+//
+//------------------------------------------------------------------------ 
+
+class Instruction : public Symbole
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- Méthodes publiques
+	virtual void effectuer() = 0;
+
+//------------------------------------------------- Surcharge d'opérateurs
+    Instruction& operator=(const Instruction & unInstruction) = delete;
+
+//-------------------------------------------- Constructeurs - destructeur
+    Instruction(const Instruction & unInstruction) = delete;
+
+    Instruction();
+
+    virtual ~Instruction();
+
+//------------------------------------------------------------------ PRIVE 
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+private:
+//------------------------------------------------------- Méthodes privées
+
+protected:
+//----------------------------------------------------- Attributs protégés
+
+private:
+//------------------------------------------------------- Attributs privés
+
+//---------------------------------------------------------- Classes amies
+
+//-------------------------------------------------------- Classes privées
+
+//----------------------------------------------------------- Types privés
+
+};
+
+//----------------------------------------- Types dépendants de <Instruction>
+
+#endif // INSTRUCTION_H
+
---
+++ prog/src/symboles/Instruction.cpp
@@ -0,0 +1,9 @@
+#include "symboles/Instruction.hpp"
+
+Instruction::Instruction():Symbole(Symbole::Types::INSTRUCTION)
+{
+}
+
+Instruction::~Instruction()
+{
+}
