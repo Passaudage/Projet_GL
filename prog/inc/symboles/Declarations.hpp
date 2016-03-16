1dc1bcbdf79c5c67fa8f8d51152c440c02a9367b
Luc Forget <luc.forget@insa-lyon.fr>
2016-03-16 17:13:19 +0100

    Declarations

---
+++ prog/inc/symboles/Declarations.hpp
@@ -0,0 +1,66 @@
+/*************************************************************************
+                           Declarations  -  description
+                             -------------------
+    début                : mer. 16 mars 2016
+*************************************************************************/
+
+//--------- Interface de la classe <Declarations> (fichier Declarations.hpp) ------
+#ifndef DECLARATIONS_H
+#define DECLARATIONS_H
+
+//--------------------------------------------------- Interfaces utilisées
+#include "Symbole.hpp"
+//------------------------------------------------------------- Constantes 
+
+//------------------------------------------------------------------ Types 
+
+//------------------------------------------------------------------------ 
+// Rôle de la classe <Declarations>
+//
+//
+//------------------------------------------------------------------------ 
+
+class Declarations : public Symbole
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- Méthodes publiques
+
+
+//------------------------------------------------- Surcharge d'opérateurs
+    Declarations& operator=(const Declarations & unDeclarations) = delete;
+
+//-------------------------------------------- Constructeurs - destructeur
+    Declarations(const Declarations & unDeclarations) = delete;
+
+    Declarations();
+
+    virtual ~Declarations();
+
+//------------------------------------------------------------------ PRIVE 
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+private:
+//------------------------------------------------------- Méthodes privées
+
+protected:
+//----------------------------------------------------- Attributs protégés
+
+private:
+//------------------------------------------------------- Attributs privés
+
+//---------------------------------------------------------- Classes amies
+
+//-------------------------------------------------------- Classes privées
+
+//----------------------------------------------------------- Types privés
+
+};
+
+//----------------------------------------- Types dépendants de <Declarations>
+
+#endif // DECLARATIONS_H
+
